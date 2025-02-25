c417fd4b835f5a705895b22d46265d65c83bf18f
Quang HUy <huypro0612@gmail.com>
2025-02-25 21:00:23 +0700

    Update cartServices.js

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -1,31 +1,29 @@
-import axios from "axios";
 import api from "../api/api";
 
 const cartService = {
-  addToCart: async (userId, items) => {
-    userId = userId || "67bb8d52aaad8702150f75fc";
-    console.log("User ID:", userId);
-    console.log("Items to add:", items);
+    addToCart: async (userId, items) => {
+        userId = userId || "67bb8d52aaad8702150f75fc";
+        console.log("User ID:", userId);
+        console.log("Items to add:", items);
+        try {
+            // Gửi toàn bộ danh sách sản phẩm lên server
+            const response = await api.post(`/cart/add`, items);
+            return response.data;
+        } catch (error) {
+            console.error("Lỗi khi thêm sản phẩm vào giỏ hàng:", error);
+            throw error;
+        }
+    },
 
-    try {
-      // Gửi toàn bộ danh sách sản phẩm lên server
-      const response = await api.post(`/cart/${userId}/add`, items);
-      return response.data;
-    } catch (error) {
-      console.error("Lỗi khi thêm sản phẩm vào giỏ hàng:", error);
-      throw error;
-    }
-  },
-
-  getCart: async (userId) => {
-    try {
-      const response = await api.get(`/cart/${userId}`);
-      return response.data;
-    } catch (error) {
-      console.error("Lỗi khi lấy giỏ hàng:", error);
-      throw error;
-    }
-  },
+    getCart: async (userId) => {
+        try {
+            const response = await api.get(`/cart`);
+            return response.data;
+        } catch (error) {
+            console.error("Lỗi khi lấy giỏ hàng:", error);
+            throw error;
+        }
+    },
 };
 
 export default cartService;
