c61fcd5811bf43816e02dd867e25c29b1208b600
lehonghai <hlehong163@gmail.com>
2025-02-25 22:10:48 +0700

    sua lai cart

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -3,13 +3,13 @@ import api from "../api/api";
 
 const cartService = {
   addToCart: async (userId, items) => {
-    userId = userId || "67bb8d52aaad8702150f75fc";
+    userId = userId ;
     console.log("User ID:", userId);
     console.log("Items to add:", items);
 
     try {
       // Gửi toàn bộ danh sách sản phẩm lên server
-      const response = await api.post(`/cart/${userId}/add`, items);
+      const response = await api.post(`/cart/add`, items);
       return response.data;
     } catch (error) {
       console.error("Lỗi khi thêm sản phẩm vào giỏ hàng:", error);
